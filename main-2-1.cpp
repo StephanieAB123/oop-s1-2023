3db7475e73d429889662ddb0d7e618f95572c1fd
StephanieAB123 <a1877567@adelaide.edu>
2023-07-28 15:41:20 +0930

    test

--- main-2-1.cpp
+++ main-2-1.cpp
@@ -6,7 +6,7 @@ int main(){
 
     int array[5] = {4,5,1,3,2};
 
-    std::cout << "The smallest number in the array is: " << min_element(array, 5) << std:: endl;
+    std::cout << min_element(array, 5) << std:: endl;
 
     return 0;
 }
